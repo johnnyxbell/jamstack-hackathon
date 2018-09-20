81e0ca85807ce4ed72aa49cb8c58baec05b89beb
Johnny Bell <hi@johnnybell.io>
2018-09-20 14:26:01 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi people</h1>
+    <p>
+      Hi, I'm Johnny Bell, a frontend engineer from Australia. Working with the
+      awesome team at Stackshare in San Francisco.
+    </p>
+  </Layout>
+)
+
+export default IndexPage
